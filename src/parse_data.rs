e441449c32d437a9a938add89b38666391b9096a
Yomnaali22 <56122735+Yomnaali22@users.noreply.github.com>
2025-03-29 13:03:55 +0200

    adjust crates imports

--- src/parse_data.rs
+++ src/parse_data.rs
@@ -1,28 +1,22 @@
 use serde::Serialize;
 use serde::de::DeserializeOwned;
-use serde_json::{from_reader, to_string_pretty};
+use serde_json::to_string_pretty;
 use std::fmt::Debug;
-use std::fs::{File, OpenOptions};
-use std::io::{BufReader, BufWriter, Result, Write};
+use std::fs::{File, read_to_string};
+use std::io::{Result, Write};
 pub struct JsonData;
 
 impl JsonData {
     pub fn process<T: DeserializeOwned + Debug + Serialize>(path: &str) -> Result<T> {
-        let file = File::open(path)?;
-        let reader = BufReader::new(file);
-        let data = from_reader(reader)?;
-        Ok(data)
+        let data = read_to_string(path)?;
+        let inventory: T = serde_json::from_str(&data)?;
+        Ok(inventory)
     }
 
-    pub fn writes<T: DeserializeOwned + Debug + Serialize>(path: &str, data: &T) -> Result<String> {
+    pub fn writes<T: DeserializeOwned + Debug + Serialize>(path: &str, data: &T) -> Result<()> {
         let update_json = to_string_pretty(data)?;
-        // Open the file for writing (truncate = overwrite)
-        let file = OpenOptions::new().write(true).truncate(true).open(path)?;
-
-        let mut writer = BufWriter::new(file);
-        writer.write(update_json.as_bytes())?;
-        writer.flush()?;
-
-        Ok("n".to_string())
+        let mut file = File::create(path)?;
+        file.write_all(update_json.as_bytes())?;
+        Ok(())
     }
 }
