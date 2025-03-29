4507dd929404183f0b0a98484450a7d08bdb2563
Yomnaali22 <56122735+Yomnaali22@users.noreply.github.com>
2025-03-29 12:57:58 +0200

    Use handle_env_variables function from the root crate (lib)

--- src/app.rs
+++ src/app.rs
@@ -1,35 +1,69 @@
+use crate::handle_env_variables;
 use crate::parse_data::JsonData;
 use crate::{Product, inventory::Inventory};
-use axum::Json;
+use crate::{PurchaseTransaction, SaleTransaction, Transaction};
 use serde_json::{Value, json};
 
-pub fn get_inventory() -> Json<Value> {
-    let inventory: Inventory = match JsonData::process("./../inventory_data.json") {
+pub fn get_inventory() -> Result<Inventory, String> {
+    let path = handle_env_variables("INVENTORY_JSON_PATH");
+    let inventory: Inventory = match JsonData::process(&path) {
         Ok(data) => data,
         Err(err) => {
             println!("Error processing inventory data: {:?}", err);
-            return Json(json!("error"));
+            return Err(format!("Error processing inventory data: {:?}", err));
         }
     };
     // Generate report and convert to JSON
-    Json(json!(inventory))
+    Ok(inventory)
 }
 
-pub fn add_product(product: &Product) -> Json<Value> {
-    let mut inventory: Inventory = match JsonData::process("./../inventory_data.json") {
+pub fn add_product(product: &Product) -> Value {
+    let path = handle_env_variables("INVENTORY_JSON_PATH");
+    let mut inventory: Inventory = match JsonData::process(&path) {
         Ok(data) => data,
         Err(err) => {
             println!("Error processing inventory data: {:?}", err);
-            return Json(json!("Error processing inventory data: "));
+            return json!("Error processing inventory data: ");
         }
     };
-    let inventory = match Inventory::modify_products(product, "add", &mut inventory) {
+    let inventory = match Inventory::modify_products(product, "add", &mut inventory, &path) {
         Ok(data) => String::from(data),
         Err(err) => {
             println!("Error adding product: {:?}", err);
-            return Json(json!("error adding product"));
+            return json!("error adding product");
         }
     };
     // Generate report and convert to JSON
-    Json(json!(inventory))
+    json!(inventory)
+}
+
+pub fn add_sale(sale_transaction: SaleTransaction) -> Value {
+    let path = handle_env_variables("INVENTORY_JSON_PATH");
+    let mut inventory = match JsonData::process(&path) {
+        Ok(inventory) => inventory,
+        Err(e) => return json!(format!("Error processing inventory data: {}", e)),
+    };
+    let total_sales = SaleTransaction::calculate_total_sales(&sale_transaction, &inventory);
+    let total_profit = SaleTransaction::calculate_profit(&sale_transaction, &inventory);
+
+    match SaleTransaction::add_transaction(&sale_transaction, &mut inventory, &path) {
+        Ok(transaction) => json!(transaction),
+        Err(e) => return json!(format!("Error processing inventory data: {}", e)),
+    };
+    return json!({ "total_sales": total_sales, "total_profit": total_profit });
+}
+
+pub fn add_purchase(purchase_transaction: PurchaseTransaction) -> Value {
+    let path = handle_env_variables("INVENTORY_JSON_PATH");
+    let mut inventory = match JsonData::process(&path) {
+        Ok(inventory) => inventory,
+        Err(e) => return json!(format!("Error processing inventory data: {}", e)),
+    };
+    let total_cost = PurchaseTransaction::calculate_total_cost(&purchase_transaction, &inventory);
+    let transaction =
+        match PurchaseTransaction::add_transaction(&purchase_transaction, &mut inventory, &path) {
+            Ok(transaction) => json!(transaction),
+            Err(e) => return json!(e),
+        };
+    json!({"total_cost": total_cost, "purchase_transaction": transaction})
 }
