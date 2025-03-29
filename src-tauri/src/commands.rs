a4b1080e35e696f5721d5a9dd1c8f0602b7de9be
Yomnaali22 <56122735+Yomnaali22@users.noreply.github.com>
2025-03-29 12:56:09 +0200

    Add commands to call from the frontend

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,16 +1,44 @@
-use axum::Json;
-use inventory_lib::{add_product, get_inventory, Inventory, Product};
-use serde_json::Value;
+use lib::{
+    add_product, add_purchase, add_sale, authenticate, get_inventory, Inventory, Product,
+    PurchaseTransaction, SaleTransaction, User,
+};
+use serde_json::{json, Value};
 use tauri::command;
 
 #[command]
 pub fn inventory() -> Value {
-    let Json(inventory) = get_inventory();
-    inventory
+    let inventory: Result<Inventory, String> = get_inventory();
+    let inventory = match inventory {
+        Ok(inventory) => inventory,
+        Err(e) => return json!(e),
+    };
+
+    json!(inventory)
 }
 
 #[command]
 pub fn add_product_to_inventory(product: Product) -> Value {
-    let Json(inventory) = add_product(&product);
+    let inventory = add_product(&product);
     inventory
+}
+
+#[command]
+pub fn login(credentials: User) -> Result<Value, String> {
+    let auth: Result<User, String> = match authenticate(&credentials.name, &credentials.password) {
+        Ok(user) => Ok(user),
+        Err(e) => return Err(format!("Login Failed: {}", e)),
+    };
+    Ok(json!(auth))
+}
+
+#[command]
+pub fn record_sale_transaction(sale_transaction: SaleTransaction) -> Value {
+    let sale = add_sale(sale_transaction);
+    sale
+}
+
+#[command]
+pub fn record_purchase_transaction(purchase_transaction: PurchaseTransaction) -> Value {
+    let purchase = add_purchase(purchase_transaction);
+    purchase
 }
