5a7d28b3b4166b8e85dfdd8c81ac85abe25a7ef4
Yomnaali22 <56122735+Yomnaali22@users.noreply.github.com>
2025-03-29 12:55:19 +0200

    Load env for development

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,10 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use dotenv::{dotenv, from_filename};
+
 fn main() {
+    from_filename(".env_dev").ok();
+    let my_var = std::env::var("INVENTORY_JSON_PATH").expect("INVENTORY_JSON_PATH must be set");
+    println!("INVENTORY_JSON_PATH: {}", my_var);
     app_lib::run();
 }
