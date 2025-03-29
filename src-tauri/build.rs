2301714b388631b4719c25fc80b534d5d4a80fc9
Yomnaali22 <56122735+Yomnaali22@users.noreply.github.com>
2025-03-29 12:54:19 +0200

    Load env for build

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,10 @@
+use dotenv::{dotenv, from_filename};
+use std::env;
+
 fn main() {
+    // load env variables from .env file
+    from_filename(".env").ok();
+    // Now you can access variables
+
     tauri_build::build();
 }
