c045f87a3ad7d565c151f957aaedcc410003a289
Yomnaali22 <56122735+Yomnaali22@users.noreply.github.com>
2025-03-31 11:49:16 +0200

    remove .env_dev from gitignore

--- src/env.rs
+++ src/env.rs
@@ -1,10 +1,9 @@
-use dotenv::dotenv;
 use lazy_static::lazy_static;
 use std::collections::HashMap;
 
 lazy_static! {
     pub static ref ENV_VARS: HashMap<String, String> = {
-        dotenv::from_filename(".env_dev").ok(); // Load your custom env file
+        dotenv::from_filename(".env").ok(); // Load your custom env file
         std::env::vars().collect()
     };
 }
